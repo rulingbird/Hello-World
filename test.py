574a5f1c758cd7417d3a18b3a856db035129ce2e
Yasin Elmore <yuxuanpeng@outlook.com>
2022-07-20 11:21:16 +0800

    a change

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-def m(x):
-    return x*x
-print (m(8))
-print(m(12))
-print("hello")
+def cs61nay(combiner, n):
+    def g(x, i = n):
+        if n == 1:
+            return x
+        def h(y):
+            k = combiner(x, y)
+            if i-2 >= 1:
+                return g(k, i-1)
+            else:
+                return k
+        return h
+    return g    
+
+from operator import sub, add, mul
+
+compose = lambda f, g: lambda x: f(g(x))
+
+print(compose(cs61nay(sub, 2), compose(cs61nay(mul, 3)(2),cs61nay(pow, 2)(10))(3))(1)(-21))
